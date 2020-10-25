006a59388dfa0b8894257bbf7313c6c4de60a3e2
Mahesh Pandekar <pmahesh6789@gmail.com>
2020-10-25 19:39:42 +0530

    Removed secure information.

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 /*
 *   TODO: Please update this configuration into .env file.
-*   NOTE: For reading environment variables from .env file required vue clie version 3 or above.
+*   NOTE: For reading environment variables from .env file required vue cli version 3 or above.
 */
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = '76f432f69a5a41b6a48f3895a3ed1c98';
+export const clientId = 'YOUR_SPOTIFY_CLIENT_ID';
 export const redirectUri = 'http://localhost:3000/redirect';
 export const scopes = [
     'user-top-read',
